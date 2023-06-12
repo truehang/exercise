4a913b9c6a715c9079cbe601f9ede6c9dd948523
Hogan Chou <hoganchous@gmail.com>
2023-06-12 01:55:14 -0400

    2D Fenwick Tree

--- tree/FenwickTree.cpp
+++ tree/FenwickTree.cpp
@@ -9,7 +9,7 @@
 
 // Least Significant Bit of i having a value of 1
 #define LSB(i) ((i) & -(i))
-
+// 一维度的BIT
 struct FenwickTree{
     vector<int> A;
     // 前i个和
@@ -97,5 +97,82 @@ public:
        return fentree.rangeSum(left, right + 1);
     }
     struct FenwickTree fentree;
+};
+*/
+
+// 二维的树状数组
+struct BIT{
+    int m, n;
+    vector<vector<int>> A;
+    int presum(int i, int j){
+        int sum = 0;
+        for(; i != 0; i -= LSB(i))
+        {
+            int c = j;
+            for(; c != 0; c -= LSB(c))
+            {
+                sum += A[i][c];
+            }
+        }
+        return sum;
+    }
+
+    void add(int i, int j, int diff)
+    {
+        for(; i < m; i += LSB(i))
+        {
+            int c = j;
+            for(; c < n; c += LSB(c))
+            {
+                A[i][c] += diff;
+            }
+        }
+    }
+
+    void init(vector<vector<int>>& matrix)
+    {
+        A = vector<vector<int>>(m, vector<int>(n));
+        for(int i = 1; i < m; ++i)
+        {
+            for(int j = 1; j < n; ++j)
+            {
+                A[i][j] += matrix[i - 1][j - 1]; // 因为j-1位置的数也会加到这里
+                int jj = j + LSB(j);
+                if(jj < n)
+                    A[i][jj] += A[i][j];
+            }
+        }
+        for(int i = 1; i < m; ++i)
+        {
+            for(int j = 1;j < n; ++j)
+            {
+                int ii = i + LSB(i);
+                if(ii < m)
+                    A[ii][j] += A[i][j];
+            }
+        }
+    }
+
+    BIT(vector<vector<int>>& matrix):m(matrix.size() + 1), n(matrix[0].size() + 1){
+        init(matrix);
+    }
+};
+
+//应用：高效求矩形区域的和
+/*
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix):ftree(matrix) {
+    }
+    
+    void update(int row, int col, int val) {
+        ftree.add(row + 1, col + 1, val - sumRegion(row, col, row, col)); // 减去range不是数
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return ftree.presum(row2 + 1, col2 + 1) - ftree.presum(row2 + 1, col1) - ftree.presum(row1, col2 + 1) + ftree.presum(row1, col1);
+    }
+private:
+    BIT ftree;
 };
 */
